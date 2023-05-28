b855956f642c32ac992ae998f7d83a26a20c3977
ManosKar840 <89093198+ManosKaramitros@users.noreply.github.com>
2023-05-28 23:52:20 +0300

    Add files via upload

---
+++ Project-code/ActivityChartsLookup/Activity-Charts-Class.java
@@ -0,0 +1,34 @@
+Activity Charts Class:
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ActivityCharts {
+    private Map<String, Integer> activityTime;
+
+    public ActivityCharts() {
+        activityTime = new HashMap<>();
+    }
+
+    public void addActivityTime(String activity, int time) {
+        activityTime.put(activity, time);
+    }
+
+    public void removeActivityTime(String activity) {
+        activityTime.remove(activity);
+    }
+
+    public int getTotalTime() {
+        int totalTime = 0;
+        for (int time : activityTime.values()) {
+            totalTime += time;
+        }
+        return totalTime;
+    }
+
+    public void displayActivities() {
+        for (Map.Entry<String, Integer> entry : activityTime.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue() + " minutes");
+        }
+    }
+}
