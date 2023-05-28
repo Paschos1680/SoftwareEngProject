ecbb16aca707c92c22a4d576500ae96e7e8b51cc
Panagopoulos Nikos <126821494+Nikospanag@users.noreply.github.com>
2023-05-28 23:47:44 +0300

    Add files via upload

---
+++ Project-code/RecipeSearchAndUpload/RecipeSearch Class.java
@@ -0,0 +1,29 @@
+RecipeSearch Class:
+
+java
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecipeSearch {
+    private List<String> recipes;
+
+    public RecipeSearch() {
+        recipes = new ArrayList<>();
+    }
+
+    public void addRecipe(String recipe) {
+        recipes.add(recipe);
+    }
+
+    public void removeRecipe(String recipe) {
+        recipes.remove(recipe);
+    }
+
+    public void displayRecipes() {
+        System.out.println("Available Recipes:");
+        for (String recipe : recipes) {
+            System.out.println("- " + recipe);
+        }
+    }
+}
---
+++ Project-code/RecipeSearchAndUpload/RecipeUpload Class.java
@@ -0,0 +1,57 @@
+RecipeUpload Class:
+
+java
+
+public class RecipeUpload {
+    private String recipeName;
+    private String instructions;
+    private List<String> ingredients;
+
+    public RecipeUpload(String recipeName, String instructions, List<String> ingredients) {
+        this.recipeName = recipeName;
+        this.instructions = instructions;
+        this.ingredients = ingredients;
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public void addIngredient(String ingredient) {
+        ingredients.add(ingredient);
+    }
+
+    public void removeIngredient(String ingredient) {
+        ingredients.remove(ingredient);
+    }
+
+    public void displayRecipe() {
+        System.out.println("Recipe: " + recipeName);
+        System.out.println("Ingredients: ");
+        for (String ingredient : ingredients) {
+            System.out.println("- " + ingredient);
+        }
+        System.out.println("Instructions: ");
+        System.out.println(instructions);
+    }
+}
