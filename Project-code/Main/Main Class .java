957f2177077ee7fedae289b4acaec01a06f7fe82
Panagopoulos Nikos <126821494+Nikospanag@users.noreply.github.com>
2023-05-28 23:49:11 +0300

    Add files via upload

---
+++ Project-code/Main/Main Class .java
@@ -0,0 +1,36 @@
+ Main Class:
+
+java
+
+public class Main {
+    public static void main(String[] args) {
+        ProfileSetup profile = new ProfileSetup("JohnDoe", 25, "Male");
+        profile.displayProfile();
+
+        RecipeUpload recipe = new RecipeUpload("Pasta Carbonara", "Instructions here...", new ArrayList<>());
+        recipe.addIngredient("Pasta");
+        recipe.addIngredient("Bacon");
+        recipe.addIngredient("Eggs");
+        recipe.displayRecipe();
+
+        RecipeSearch recipeSearch = new RecipeSearch();
+        recipeSearch.addRecipe("Pasta Carbonara");
+        recipeSearch.addRecipe("Chicken Curry");
+        recipeSearch.displayRecipes();
+
+        NutritionMonitoring nutrition = new NutritionMonitoring();
+        nutrition.addIntake("Protein", 50);
+        nutrition.addIntake("Carbohydrates", 100);
+        nutrition.displayIntake();
+
+        ActivityCharts activityCharts = new ActivityCharts();
+        activityCharts.addActivityTime("Running", 30);
+        activityCharts.addActivityTime("Swimming", 45);
+        activityCharts.displayActivities();
+
+        PenaltyAdministration penaltyAdmin = new PenaltyAdministration();
+        penaltyAdmin.addPenalty("Late submission");
+        penaltyAdmin.addPenalty("Incomplete work");
+        penaltyAdmin.displayPenalties();
+    }
+}
