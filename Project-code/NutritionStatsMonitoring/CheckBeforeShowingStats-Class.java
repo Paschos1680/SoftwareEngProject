79c2b077dc1635917326266c913873455485548e
Panagopoulos Nikos <126821494+Nikospanag@users.noreply.github.com>
2023-06-09 16:40:03 +0300

    Add files via upload

---
+++ Project-code/NutritionStatsMonitoring/CheckBeforeShowingStats-Class.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+
+public class CheckBeforeShowingStats {
+    
+    public CheckBeforeShowingStats() {
+        
+    }
+    
+    public boolean checkIfAIModeIsEnabled() {
+        // Check if the AI mode is enabled
+    
+        return false;
+    }
+    
+    public void checkActivityStats(Date calendarDay) {
+       
+    }
+    
+    public void checkPreviousNutritionRecordings(Date calendarDay) {
+        // Check the previous nutrition recordings for the specified calendar day
+       
+}
