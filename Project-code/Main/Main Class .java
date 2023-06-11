7c1a4a7f890553d8aa702c62052a3a9ba0ba1669
ManosKar840 <89093198+ManosKaramitros@users.noreply.github.com>
2023-06-12 00:29:40 +0300

    Update Main Class .java

--- Project-code/Main/Main Class .java
+++ Project-code/Main/Main Class .java
@@ -1,34 +1,29 @@
-
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
 
 public class Main {
     public static void main(String[] args) {
-        ProfileSetup profile = new ProfileSetup("JohnDoe", 25, "Male");
-        profile.displayProfile();
+        
+        JFrame frame = new JFrame("HealthHive");
 
-        RecipeUpload recipe = new RecipeUpload("Pasta Carbonara", "Instructions here...", new ArrayList<>());
-        recipe.addIngredient("Pasta");
-        recipe.addIngredient("Bacon");
-        recipe.addIngredient("Eggs");
-        recipe.displayRecipe();
+ 
+        JLabel welcomeLabel = new JLabel("Welcome to HealthHive!");
+        welcomeLabel.setHorizontalAlignment(JLabel.CENTER);
+        welcomeLabel.setForeground(Color.BLACK); 
+        welcomeLabel.setFont(new Font("Arial", Font.BOLD, 24)); 
 
-        RecipeSearch recipeSearch = new RecipeSearch();
-        recipeSearch.addRecipe("Pasta Carbonara");
-        recipeSearch.addRecipe("Chicken Curry");
-        recipeSearch.displayRecipes();
+        
+        frame.getContentPane().setBackground(Color.YELLOW);
 
-        NutritionMonitoring nutrition = new NutritionMonitoring();
-        nutrition.addIntake("Protein", 50);
-        nutrition.addIntake("Carbohydrates", 100);
-        nutrition.displayIntake();
+   
+        frame.getContentPane().add(welcomeLabel);
 
-        ActivityCharts activityCharts = new ActivityCharts();
-        activityCharts.addActivityTime("Running", 30);
-        activityCharts.addActivityTime("Swimming", 45);
-        activityCharts.displayActivities();
-
-        PenaltyAdministration penaltyAdmin = new PenaltyAdministration();
-        penaltyAdmin.addPenalty("Late submission");
-        penaltyAdmin.addPenalty("Incomplete work");
-        penaltyAdmin.displayPenalties();
+        
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
