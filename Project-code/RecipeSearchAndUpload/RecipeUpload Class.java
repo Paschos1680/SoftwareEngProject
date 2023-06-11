5d6a1f99e673dddeb6b880737455c203badd702e
ManosKar840 <89093198+ManosKaramitros@users.noreply.github.com>
2023-06-11 23:43:43 +0300

    Update RecipeUpload Class.java

--- Project-code/RecipeSearchAndUpload/RecipeUpload Class.java
+++ Project-code/RecipeSearchAndUpload/RecipeUpload Class.java
@@ -1,54 +1,45 @@
+import java.time.LocalDateTime;
 
-public class RecipeUpload {
-    private String recipeName;
-    private String instructions;
-    private List<String> ingredients;
+public class RecipeUpload extends RecipeSearch {
+    private String name;
+    private String ingredients;
+    private String process;
+    private double calories;
 
-    public RecipeUpload(String recipeName, String instructions, List<String> ingredients) {
-        this.recipeName = recipeName;
-        this.instructions = instructions;
+    public RecipeUpload(String searchedRecipeName, float averageRating, int ratingCount, String createdBy, LocalDateTime creationDate,
+                        String name, String ingredients, String process, double calories) {
+        super(searchedRecipeName, averageRating, ratingCount, createdBy, creationDate);
+        this.name = name;
         this.ingredients = ingredients;
+        this.process = process;
+        this.calories = calories;
     }
 
-    public String getRecipeName() {
-        return recipeName;
+    public void createRecipe(String name) {
+        System.out.println("Creating recipe: " + name);
     }
 
-    public void setRecipeName(String recipeName) {
-        this.recipeName = recipeName;
+    public void submitRecipe() {
+        System.out.println("Submitting recipe: " + name);
     }
 
-    public String getInstructions() {
-        return instructions;
+    public void updateIngredients() {
+        System.out.println("Updating ingredients for recipe: " + ingredients);
     }
 
-    public void setInstructions(String instructions) {
-        this.instructions = instructions;
+    public void updateProcess() {
+        System.out.println("Updating process for recipe: " + process);
     }
 
-    public List<String> getIngredients() {
-        return ingredients;
+    public void calculateCalories() {
+        System.out.println("Calculating calories for recipe: " + name);
     }
 
-    public void setIngredients(List<String> ingredients) {
-        this.ingredients = ingredients;
+    public String getCreatedBy(String username) {
+        return createdBy;
     }
 
-    public void addIngredient(String ingredient) {
-        ingredients.add(ingredient);
-    }
-
-    public void removeIngredient(String ingredient) {
-        ingredients.remove(ingredient);
-    }
-
-    public void displayRecipe() {
-        System.out.println("Recipe: " + recipeName);
-        System.out.println("Ingredients: ");
-        for (String ingredient : ingredients) {
-            System.out.println("- " + ingredient);
-        }
-        System.out.println("Instructions: ");
-        System.out.println(instructions);
+    public LocalDateTime getCreationDate() {
+        return creationDate;
     }
 }
