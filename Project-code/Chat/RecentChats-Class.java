103fc7a7ce5f663e98f29504c0dc0ca54906da64
ManosKar840 <89093198+ManosKaramitros@users.noreply.github.com>
2023-06-11 23:59:09 +0300

    Update RecentChats-Class.java

--- Project-code/Chat/RecentChats-Class.java
+++ Project-code/Chat/RecentChats-Class.java
@@ -1,94 +1,44 @@
+public class RecentChats {
+    private List<String> recentChats;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-public class Chat {
-    private List<String> recentConversations;
-    private List<String> searchList;
-    private String currentUser;
-
-    public Chat() {
-        recentConversations = new ArrayList<>();
-        searchList = new ArrayList<>();
+    public RecentChats() {
+        recentChats = new ArrayList<>();
     }
 
-    public void startChat() {
-        displayRecentConversations();
-        displaySearchList();
-        selectUser();
-        personalChatPage();
+    public void addChat(String chat) {
+        recentChats.add(chat);
     }
 
-    private void displayRecentConversations() {
-        System.out.println("Recent Conversations:");
-        for (String conversation : recentConversations) {
-            System.out.println("- " + conversation);
+    public void removeChat(int index) {
+        if (index >= 0 && index < recentChats.size()) {
+            recentChats.remove(index);
+        } else {
+            System.out.println("Eror! Cant Remove Chat.");
         }
     }
 
-    private void displaySearchList() {
-        System.out.println("Search List:");
-        for (String user : searchList) {
-            System.out.println("- " + user);
+    public String getRecentChats() {
+        StringBuilder sb = new StringBuilder();
+        for (String chat : recentChats) {
+            sb.append(chat).append("\n");
         }
+        return sb.toString();
     }
 
-    private void selectUser() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Select a user from recent conversations or search list:");
-        String selectedUser = scanner.nextLine();
-        currentUser = selectedUser;
-    }
-
-    private void personalChatPage() {
-        System.out.println("Personal Chat with " + currentUser);
-        System.out.println("1. Compose new message");
-        System.out.println("2. View messages");
-
-        Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
-
-        switch (choice) {
-            case 1:
-                composeMessage();
-                break;
-            case 2:
-                viewMessages();
-                break;
-            default:
-                System.out.println("Invalid choice.");
-                break;
+    public String displayRecentChats() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Recent Chats:\n");
+        for (int i = 0; i < recentChats.size(); i++) {
+            sb.append(i + 1).append(". ").append(recentChats.get(i)).append("\n");
         }
+        return sb.toString();
     }
 
-    private void composeMessage() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Compose your message:");
-        String message = scanner.nextLine();
-        sendMessage(message);
-    }
-
-    private void sendMessage(String message) {
-        System.out.println("Message sent: " + message);
-        // Update personal chat page
-        personalChatPage();
-    }
-
-    private void viewMessages() {
-
-        personalChatPage();
-    }
-
-    public void addRecentConversation(String conversation) {
-        recentConversations.add(conversation);
-    }
-
-    public void addSearchUser(String user) {
-        searchList.add(user);
-    }
-
-    public void closeChat() {
-        System.out.println("Chat closed.");
+    public void editChat(int index, String newContent) {
+        if (index >= 0 && index < recentChats.size()) {
+            recentChats.set(index, newContent);
+        } else {
+            System.out.println("Eror. Cnan't edit Chat!. ");
+        }
     }
 }
