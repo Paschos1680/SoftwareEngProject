58412bbdbe23a455c061af67f45c59db11a0b598
ManosKar840 <89093198+ManosKaramitros@users.noreply.github.com>
2023-06-12 00:51:32 +0300

    Add files via upload

---
+++ Project-code/WorkoutConfigurationAndChartsUpdate/ActivityStatsDayClass.java
@@ -0,0 +1,30 @@
+import java.util.Date;
+
+public class ActivityStatsDay extends Training {
+    private String activityStatsData; 
+    
+    public ActivityStatsDay(String trainingConfiguration) {
+        super(trainingConfiguration);
+        activityStatsData = ""; 
+    }
+    
+    public boolean checkActivityStats(Date calendarDay) {
+       
+        return true;
+    }
+    
+    public void getActivityStats(Date calendarDay) {
+        
+        System.out.println("Retrieving activity stats for: " + calendarDay);
+        
+     
+        
+        // Update activity stats data
+        activityStatsData = "Steps: 10000, Distance: 5.0 km, Calories Burned: 500";
+    }
+    
+   
+    public String getActivityStatsData() {
+        return activityStatsData;
+    }
+}
