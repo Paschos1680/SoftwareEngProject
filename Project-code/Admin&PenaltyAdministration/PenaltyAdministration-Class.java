5d24d316dcf95e6f9ff8398aaa28b06477b626d6
ManosKar840 <89093198+ManosKaramitros@users.noreply.github.com>
2023-06-11 17:18:04 +0300

    Update PenaltyAdministration-Class.java

--- Project-code/Admin&PenaltyAdministration/PenaltyAdministration-Class.java
+++ Project-code/Admin&PenaltyAdministration/PenaltyAdministration-Class.java
@@ -1,27 +1,77 @@
-
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class PenaltyAdministration {
+    private String content;
+    private String postAuthor;
+    private int offensivePostsCount;
+    private int strikeCount;
+    private int blockCount;
+    private boolean permanentBan;
+    private int suspensionDays;
+    private int banDuration;
+    private String userHistory;
     private List<String> penalties;
 
     public PenaltyAdministration() {
         penalties = new ArrayList<>();
     }
 
+    public void deleteOffensivePost(String content) {
+        System.out.println("Deleting offensive post with content: " + content);
+        
+    }
+
+    public void giveWarning(String author) {
+        System.out.println("Giving a warning to the author: " + author);
+       
+    }
+
+    public void suspendPosting(int banDuration) {
+        System.out.println("Suspending posting for " + banDuration + " days");
+        
+    }
+
+    public void banUser(String author) {
+        System.out.println("Banning the user: " + author);
+        
+    }
+
+    public void deleteAccount(String author) {
+        System.out.println("Deleting the account of the user: " + author);
+        
+    }
+
+    public void removePostFromCommunity(CommunityPage post) {
+        System.out.println("Removing post from the community");
+        
+    }
+
+    public String getOffensivePosts(String userHistory) {
+        System.out.println("Retrieving offensive posts from user's history");
+        return null; 
+    }
+
+    public int getStrikeCount(int strikeCount) {
+        System.out.println("Retrieving strike count: " + strikeCount);
+        return 0; 
+    }
+
+    public int getBlockCount(int blockCount) {
+        System.out.println("Retrieving block count: " + blockCount);
+        return 0; 
+    }
+
+    public boolean hasPermanentBan() {
+        System.out.println("Checking if the user has a permanent ban");
+        return permanentBan;
+    }
+
     public void addPenalty(String penalty) {
         penalties.add(penalty);
     }
 
-    public void removePenalty(String penalty) {
-        penalties.remove(penalty);
-    }
-
-    public void displayPenalties() {
-        System.out.println("Penalties:");
-        for (String penalty : penalties) {
-            System.out.println("- " + penalty);
-        }
+    public List<String> getPenalties() {
+        return penalties;
     }
 }
