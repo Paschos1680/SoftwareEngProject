c4d1399d29052122f49a680d8948281eaefb1f76
ManosKar840 <89093198+ManosKaramitros@users.noreply.github.com>
2023-06-12 00:46:29 +0300

    Update WeeklyWinnerRequest-Class.java

--- Project-code/WeeklyWinner/WeeklyWinnerRequest-Class.java
+++ Project-code/WeeklyWinner/WeeklyWinnerRequest-Class.java
@@ -1,10 +1,4 @@
 public class WeeklyWinnerRequest {
-    public boolean checkWeeklyWinnerAvailability() {
-        LocalDate currentDate = LocalDate.now();
-        LocalDate weeklyWinnerDate = LocalDate.of(2023, 5, 30);
-        return currentDate.isEqual(weeklyWinnerDate);
-    }
-
     public String weeklyWinnerParticipationRequest() {
         return "Your participation request for the weekly winner has been received.";
     }
