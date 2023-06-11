82800f13fd85195a0bd4c71fe90bfd6421de613c
Panagopoulos Nikos <126821494+Nikospanag@users.noreply.github.com>
2023-06-11 19:30:50 +0300

    Update Map-Class.java

--- Project-code/MapWokrout/Map-Class.java
+++ Project-code/MapWokrout/Map-Class.java
@@ -1,39 +1,77 @@
 
 
-import java.util.HashMap;
+public class MapTraining {
+    private String mapPosition;
+    private String startingPoint;
 
-public class Map<K, V> {
-    private HashMap<K, V> map;
-
-    public Map() {
-        map = new HashMap<>();
+    public MapTraining(String startingPoint) {
+        this.startingPoint = startingPoint;
     }
 
-    public void put(K key, V value) {
-        map.put(key, value);
+    public void locateStartingPosition(String startingPoint) {
+        this.startingPoint = startingPoint;
+
+        if (GPS.isAvailable()) {
+            GPS.startTracking();
+            GPS.setLocationListener((location) -> {
+
+                System.out.println("Starting position located: " + startingPoint);
+            });
+        } else {
+            System.out.println("GPS not available. Unable to locate starting position.");
+        }
     }
 
-    public V get(K key) {
-        return map.get(key);
+    public void startMapTraining() {
+        System.out.println("Map training started. Follow the route on the map.");
+
+        if (GPS.isAvailable()) {
+            GPS.startTracking();
+            GPS.setLocationListener((location) -> {
+
+            });
+        } else {
+            System.out.println("GPS not available. Map training cannot be started.");
+        }
     }
 
-    public void remove(K key) {
-        map.remove(key);
+    public void finishMapTraining() {
+        System.out.println("Map training finished. Congratulations!");
+
+        if (GPS.isTracking()) {
+            GPS.stopTracking();
+            GPS.setLocationListener(null);
+
+        } else {
+            System.out.println("GPS tracking not active. Map training cannot be finished.");
+        }
     }
 
-    public boolean containsKey(K key) {
-        return map.containsKey(key);
+    public void updateActivityStatistics(ActivityStatsDay activityStats) {
+
+        activityStats.incrementDistanceTraveled(GPS.getTotalDistance());
+        activityStats.incrementCaloriesBurned(GPS.getCaloriesBurned());
+        System.out.println("Activity statistics updated.");
     }
 
-    public boolean containsValue(V value) {
-        return map.containsValue(value);
+    public boolean checkFriendsRoutes() {
+
+        boolean friendsRoutesAvailable = Friends.getSharedRoutes().size() > 0;
+        return friendsRoutesAvailable;
     }
 
-    public int size() {
-        return map.size();
+    public void publishRouteOnMap() {
+
+        MapRoute userRoute = GPS.getRoute();
+        userRoute.publish();
+        Friends.shareRoute(userRoute);
+        System.out.println("Route published on the map.");
     }
 
-    public void clear() {
-        map.clear();
+    public void showActivityCharts(ActivityChartsDay activityCharts) {
+
+        activityCharts.generateCharts();
+        activityCharts.display();
+        System.out.println("Activity charts displayed.");
     }
 }
